0aaa74b28adada1ec0e7ddaab9a32c90150e938c
B0BBY-du-bouk <57114303+B0BBY-du-bouk@users.noreply.github.com>
2019-11-17 14:27:30 +0100

    better

--- tableau choix ligne colonne.py
+++ tableau choix ligne colonne.py
@@ -1,12 +1,32 @@
 testta=[]
 nombredeligne=input("entre nb ligne")
-
+print(nombredeligne)
 nombredecolonne=input("entre nb colonne")
 print(nombredecolonne)
-for j in range(1, int(nombredeligne)):
+for j in range(0, int(nombredeligne)):
     tampon=[]
     for i in range(int(nombredecolonne)):
         tampon.append("_")
     testta.append(tampon)
-for j in range(0, int(nombredeligne)-1):
+for j in range(0, int(nombredeligne)):
     print(testta[j])
+nombredeligne=input("changer le nombre de ligne c'est un ordre")
+print(nombredeligne)
+for j in range(0, int(nombredeligne)):
+    tampon=[]
+    for i in range(int(nombredecolonne)):
+        tampon.append("_")
+    testta.append(tampon)
+for j in range(0, int(nombredeligne)):
+    print(testta[j])
+testta=[]
+nombredecolonne=input("et maintenant les colonnes")
+print(nombredecolonne)
+for j in range(0, int(nombredeligne)):
+    tampon=[]
+    for i in range(int(nombredecolonne)):
+        tampon.append("_")
+    testta.append(tampon)
+for j in range(0, int(nombredeligne)):
+    print(testta[j])
+
