d15c1a70a9d24739b1ead998438456092144aaad
B0BBY-du-bouk <57114303+B0BBY-du-bouk@users.noreply.github.com>
2020-02-29 19:46:26 +0100

    grille evo1

--- grille robe.py
+++ grille robe.py
@@ -8,3 +8,14 @@ cani.pack()
 for j in range (1, 10):
     cani.create_line(100*j,100,100*j,900)
     cani.create_line(100,100*j,900,100*j)
+
+testta=[]
+
+for j in range(0,9):
+    tampon=[]
+    for i in range(0,9):
+        tampon.append("_")
+    testta.append(tampon)
+for j in range(0,9):
+    print(testta[j])
+
