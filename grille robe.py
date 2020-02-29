f6ba90e9247a47c2eb44fba0cafe03630c4b24bb
B0BBY-du-bouk <57114303+B0BBY-du-bouk@users.noreply.github.com>
2020-02-29 19:20:45 +0100

    debut de grille

--- grille robe.py
+++ grille robe.py
@@ -2,6 +2,9 @@ from tkinter import*
 
 master = Tk()
 
-cani = Canvas(master, width=200, height=100)
+cani = Canvas(master, width=900, height=900)
 cani.pack()
-cani.create_line(10,10,30,30)
+#cani.create_line(100,0,100,900)
+
+for j in range (0, 8):
+    cani.create_line(100*j,50*j,100+(100*j),50+(50*j))
