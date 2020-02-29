1acfc81cdeb8b6cf8578a0b6cb1ce3b7034556b8
B0BBY-du-bouk <57114303+B0BBY-du-bouk@users.noreply.github.com>
2020-02-29 19:50:54 +0100

    GO!

--- grille robe.py
+++ grille robe.py
@@ -14,8 +14,13 @@ testta=[]
 for j in range(0,9):
     tampon=[]
     for i in range(0,9):
-        tampon.append("_")
+        tampon.append(0)
     testta.append(tampon)
 for j in range(0,9):
     print(testta[j])
 
+testta[2][2]=1
+testta[3][3]=2
+
+for j in range(0,9):
+    print(testta[j])
