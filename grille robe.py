90d63e08e5fe42b83df4e92a05eead40c045ee1c
B0BBY-du-bouk <57114303+B0BBY-du-bouk@users.noreply.github.com>
2020-03-03 21:47:18 +0100

    blabla

--- grille robe.py
+++ grille robe.py
@@ -17,22 +17,21 @@ for j in range(0,9):
     for i in range(0,9):
         tampon.append(0)
     testta.append(tampon)
-for j in range(0,9):
-    print(testta[j])
+#for j in range(0,9):
+    #print(testta[j])
 
 testta[2][2]=1
 testta[3][3]=2
 testta[4][4]=2
 
-for j in range(0,9):
-    print(testta[j])
-
-#cani.create_oval(250,250,350,350,fill='black')
-#cani.create_oval(350,350,450,450,fill='white')
+#for j in range(0,9):
+    #print(testta[j])
 
 for j in range (0,9):
     for i in range (0,9):
         if testta[j][i]>0:
-           cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='black')
-        if testta[j][i]>1:
-            cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='white')
+            if testta[j][i]==2:
+                cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='white')
+            else:
+                cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='black')
+      
