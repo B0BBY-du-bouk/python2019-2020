cd4d96de2262770806c646a00ac64838b63e6ee0
B0BBY-du-bouk <57114303+B0BBY-du-bouk@users.noreply.github.com>
2020-03-05 20:29:28 +0100

    Update grille robe.py

--- grille robe.py
+++ grille robe.py
@@ -15,27 +15,28 @@ cani.create_rectangle(100,100,900,900,fill='grey60')
 
 afficheGrille(cani)
 
-for j in range(0,9):
-    ligne=[]
-    for i in range(0,9):
-        ligne.append(0)
-    tableau.append(ligne)
-#for j in range(0,9):
-    #print(tableau[j])
-
+def creertableau():
+    for j in range(0,9):
+        ligne=[]
+        for i in range(0,9):
+            ligne.append(0)
+        tableau.append(ligne)
+    #for j in range(0,9):
+        #print(tableau[j])
+creertableau()
 tableau[2][2]=1
 tableau[3][3]=2
 tableau[4][4]=2
 
 #for j in range(0,9):
     #print(tableau[j])
-
-for j in range (0,9):
-    for i in range (0,9):
-        if tableau[j][i]>0:
-            if tableau[j][i]==2:
-                cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='white')
-            else:
-                cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='black')
-      
-
+def affichagepions():
+    for j in range (0,9):
+        for i in range (0,9):
+            if tableau[j][i]>0:
+                if tableau[j][i]==2:
+                    cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='white')
+                else:
+                    cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='black')
+          
+affichagepions()
