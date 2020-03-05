2c8bcf1f7d38b2bcd6fbc28c18cd9bc8bc7ef8b3
B0BBY-du-bouk <57114303+B0BBY-du-bouk@users.noreply.github.com>
2020-03-05 20:21:22 +0100

    exemple fonction

--- grille robe.py
+++ grille robe.py
@@ -1,37 +1,41 @@
 from tkinter import*
 
+def afficheGrille(can):
+    for j in range (1, 10):
+        can.create_line(100*j,100,100*j,900)
+        can.create_line(100,100*j,900,100*j)
+
+tableau=[]
 master = Tk()
 
 cani = Canvas(master, width=1000, height=1000,bg='grey80')
 cani.pack()
 cani.create_rectangle(100,100,900,900,fill='grey60')
 
-for j in range (1, 10):
-    cani.create_line(100*j,100,100*j,900)
-    cani.create_line(100,100*j,900,100*j)
 
-testta=[]
+afficheGrille(cani)
 
 for j in range(0,9):
-    tampon=[]
+    ligne=[]
     for i in range(0,9):
-        tampon.append(0)
-    testta.append(tampon)
+        ligne.append(0)
+    tableau.append(ligne)
 #for j in range(0,9):
-    #print(testta[j])
+    #print(tableau[j])
 
-testta[2][2]=1
-testta[3][3]=2
-testta[4][4]=2
+tableau[2][2]=1
+tableau[3][3]=2
+tableau[4][4]=2
 
 #for j in range(0,9):
-    #print(testta[j])
+    #print(tableau[j])
 
 for j in range (0,9):
     for i in range (0,9):
-        if testta[j][i]>0:
-            if testta[j][i]==2:
+        if tableau[j][i]>0:
+            if tableau[j][i]==2:
                 cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='white')
             else:
                 cani.create_oval((j*100+50),(j*100+50),(j*100+150),(j*100+150),fill='black')
       
+
